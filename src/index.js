b6abd4fd14ad1a795a785f8eca9a5b272ad1dbf7
Anastasia <ruzavinaanastasiya1@gmail.com>
2026-07-30 00:51:21 +0300

    chore: add project structure and file parser
    - Add src/index.js with genDiff function (file reading and parsing)
    - Add bin/gendiff.js (empty, to be implemented later)
    - Configure package.json with bin field and @hexlet/code name

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import yaml from 'js-yaml'
+
+const genDiff = (file1, file2, format) => {
+  const content1 = fs.readFileSync(file1, 'utf8')
+  const content2 = fs.readFileSync(file2, 'utf8')
+
+  let data1
+  let data2
+
+  if (file1.endsWith('.json')) {
+    data1 = JSON.parse(content1)
+  } else if (file1.endsWith('.yml') || file1.endsWith('.yaml')) {
+    data1 = yaml.load(content1)
+  } else {
+    throw new Error(`Unsupported file format: ${file1}`)
+  }
+
+  if (file2.endsWith('.json')) {
+    data2 = JSON.parse(content2)
+  } else if (file2.endsWith('.yml') || file2.endsWith('.yaml')) {
+    data2 = yaml.load(content2)
+  } else {
+    throw new Error(`Unsupported file format: ${file2}`)
+  }
+
+  return 'Difference will be here'
+}
+
+export default genDiff
